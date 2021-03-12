8c51fceafaa3b21d08c769e2cbe429a2834e04c6
conorog247 <70701680+conorog247@users.noreply.github.com>
2021-03-12 13:39:30 +0000

    Add files via upload

---
+++ RoomDao.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.ObjectModel;
+using SomerenModel;
+
+
+namespace SomerenDAL
+{
+    public class RoomDao : BaseDao
+    {
+        public List<Room> GetAllRooms()
+        {
+            string query = "SELECT Number, Capacity, Type FROM [Room]";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+
+        private List<Room> ReadTables(DataTable dataTable)
+        {
+            List<Room> rooms = new List<Room>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Room room = new Room()
+                {
+                    Number = (int)dr["Number"],
+                    Capacity = (int)dr["Capacity"],
+                    Type = (bool)dr["Type"]
+                };
+                rooms.Add(room);
+            }
+            return rooms;
+        }
+    }
+}
